d5ebd952b9c9a83a446bdf9a5fc213efcc8dcd0e
Helber Cisilio <h.cisilio@live.com>
2023-12-21 00:28:16 -0300

    melhorias de funcionalidades

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,30 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        local builtin = require("telescope.builtin")
-        -- local actions = require("telescope.actions")
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        -- vim.keymap.set('n', '<C-j>', actions.move_selection_next)
-        -- vim.keymap.set('n', '<C-k>', actions.move_selection_previous)
-      end
-    }
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		  dependencies = { 'nvim-lua/plenary.nvim' },
+		  config = function()
+			local builtin = require("telescope.builtin")
+			-- local actions = require("telescope.actions")
+			vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			-- vim.keymap.set('n', '<C-j>', actions.move_selection_next)
+			-- vim.keymap.set('n', '<C-k>', actions.move_selection_previous)
+		  end
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup({
+			  extensions = {
+				["ui-select"] = {
+				  require("telescope.themes").get_dropdown {
+					-- even more opts
+				  }
+				}
+			  }
+			})
+			require("telescope").load_extension("ui-select")
+		end
+	},
+}
