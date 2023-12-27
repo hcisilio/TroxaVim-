6ca682237f34f07c7e2814ad5a167d72834ba85f
Helber Cisilio <h.cisilio@live.com>
2023-12-27 00:46:08 -0300

    Adiconado atalho para formatar conforme warning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,14 +19,15 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.pylsp.setup({})
 
-			vim.keymap.set('n', 'lk', vim.diagnostic.goto_prev)
-			vim.keymap.set('n', 'lj', vim.diagnostic.goto_next)
+			vim.keymap.set('n', '<leader>lk', vim.diagnostic.goto_prev)
+			vim.keymap.set('n', '<leader>lj', vim.diagnostic.goto_next)
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
 			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set({ 'n', 'v' }, '<leader>lf', vim.lsp.buf.format, {})
 		end
 	}
 }
