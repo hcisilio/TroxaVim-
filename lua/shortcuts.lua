72ca7b75228ca9cc6d05b3216ff66733c96bc318
Helber Cisilio <h.cisilio@live.com>
2023-12-31 01:16:46 -0300

    Atalho para mover linhas

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -7,3 +7,8 @@ vim.keymap.set('n', '\\', ':vsplit<CR>')
 vim.keymap.set('n', '|', ':split<CR>')
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
